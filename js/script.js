f26a8e2710a7c02e14d05841acdbe7346e5b194b
Marina Triguk <mar22126806@yandex.ru>
2021-11-04 00:06:05 +0300

    Sprint 6

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const photoCardTemplate = document.querySelector('#photo-card-template');
 const photoGrid = document.querySelector('.photo-grid');
 const profileEditButton = document.querySelector('.profile__edit-button');
 const profileFormPopup = document.querySelector('#profile-form-popup');
+const profileForm = profileFormPopup.querySelector('.popup__form');
 const profileName = document.querySelector('.profile__name');
 const profilePersonalInfo = document.querySelector('.profile__personal-info');
 const inputName = document.querySelector('#input-name');
@@ -9,17 +10,31 @@ const inputPersonalInfo = document.querySelector('#input-personal-info');
 const popupCloseIcons = document.querySelectorAll('.popup__close-icon');
 const addCardButton = document.querySelector('.profile__add-button');
 const placeFormPopup = document.querySelector('#place-form-popup');
+const placeForm = placeFormPopup.querySelector('.popup__form');
 const inputPlaceName = document.querySelector('#input-place-name');
 const inputPlaceImgSrc = document.querySelector('#input-place-img-src');
 const imagePopup = document.querySelector('#image-popup');
 const imagePopupImg = document.querySelector('.popup__image');
+const popupList = Array.from(document.querySelectorAll('.popup'));
+const validationSettings = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  inputTouchedClass: 'popup__input_touched',
+  submitButtonSelector: '.popup__button-submit',
+  inactiveButtonClass: 'popup__button-submit_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorVisibleClass: 'form__input-error_active'
+};
+let openedPopup = null;
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
+  openedPopup = popup;
 }
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
+  openedPopup = null;
 }
 
 
@@ -59,19 +74,23 @@ initialCards.forEach(function(card) {
 profileEditButton.addEventListener(
   'click',
   function () {
-    openPopup(profileFormPopup);
     inputName.value = profileName.textContent;
     inputPersonalInfo.value = profilePersonalInfo.textContent;
+    prepareFormForUserInput(profileForm, validationSettings);
+    openPopup(profileFormPopup);
   }
 );
 
-profileFormPopup.querySelector('.popup__form').addEventListener(
+profileForm.addEventListener(
   'submit',
   function (evt) {
     evt.preventDefault();
-    profileName.textContent = inputName.value;
-    profilePersonalInfo.textContent = inputPersonalInfo.value;
-    closePopup(profileFormPopup);
+    updateFormValidity(profileForm, validationSettings);
+    if (formIsValid(profileForm, validationSettings)) {
+      profileName.textContent = inputName.value;
+      profilePersonalInfo.textContent = inputPersonalInfo.value;
+      closePopup(profileFormPopup);
+    }
   }
 )
 
@@ -88,20 +107,45 @@ popupCloseIcons.forEach(function(closePopupButton) {
 addCardButton.addEventListener(
   'click',
   function () {
+    placeForm.reset();
+    prepareFormForUserInput(placeForm, validationSettings);
     openPopup(placeFormPopup);
   }
 );
 
-placeFormPopup.querySelector('.popup__form').addEventListener(
+placeForm.addEventListener(
   'submit',
   function (evt) {
     evt.preventDefault();
-    addCard(inputPlaceName.value, inputPlaceImgSrc.value, true);
-    inputPlaceName.value = '';
-    inputPlaceImgSrc.value = '';
-    closePopup(placeFormPopup);
+    updateFormValidity(placeForm, validationSettings);
+    if (formIsValid(placeForm, validationSettings)) {
+      addCard(inputPlaceName.value, inputPlaceImgSrc.value, true);
+      closePopup(placeFormPopup);
+    }
   }
 )
 
+popupList.forEach(
+  (popup) => {
+    popup.addEventListener(
+      'click',
+      (evt) => {
+        if (evt.target === popup) {
+          closePopup(popup);
+        }
+      }
+    );
+  }
+);
 
 
+document.addEventListener(
+  'keydown',
+  (evt) => {
+    if (evt.key === 'Escape' && openedPopup !== null) {
+      closePopup(openedPopup);
+    }
+  }
+);
+
+enableValidation(validationSettings);
---
+++ js/validate.js
@@ -0,0 +1,86 @@
+const showInputError = (formElement, inputElement, errorMessage, validationSettings) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(validationSettings.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(validationSettings.errorVisibleClass);
+};
+
+const hideInputError = (formElement, inputElement, validationSettings) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(validationSettings.inputErrorClass);
+  errorElement.classList.remove(validationSettings.errorVisibleClass);
+  errorElement.textContent = '';
+};
+const inputIsTouched = (inputElement, validationSettings) => inputElement.classList.contains(validationSettings.inputTouchedClass);
+
+const setInputTouchedState = (inputElement, validationSettings) => {
+  if (!inputIsTouched(inputElement, validationSettings)) {
+    inputElement.classList.add(validationSettings.inputTouchedClass);
+  }
+}
+
+const clearInputTouchedState = (inputElement, validationSettings) => {
+  if (inputIsTouched(inputElement, validationSettings)) {
+    inputElement.classList.remove(validationSettings.inputTouchedClass);
+  }
+}
+
+const clearAllFormInputsTouchedState = (formElement, validationSettings) => {
+  const inputList = Array.from(formElement.querySelectorAll(validationSettings.inputSelector));
+  inputList.forEach((inputElement) => clearInputTouchedState(inputElement, validationSettings));
+}
+
+
+const updateInputValidity = (formElement, inputElement, validationSettings) => {
+  if (!inputElement.validity.valid && inputIsTouched(inputElement, validationSettings)) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, validationSettings);
+  } else {
+    hideInputError(formElement, inputElement, validationSettings);
+  }
+};
+
+const formIsValid = (formElement, validationSettings) => {
+  const inputList = Array.from(formElement.querySelectorAll(validationSettings.inputSelector));
+  return inputList.every((inputElement) => inputElement.validity.valid);
+}
+
+const updateButtonState = (formElement, validationSettings) => {
+  const buttonElement = formElement.querySelector(validationSettings.submitButtonSelector);
+  if (formIsValid(formElement, validationSettings)) {
+    buttonElement.classList.remove(validationSettings.inactiveButtonClass);
+  } else {
+    buttonElement.classList.add(validationSettings.inactiveButtonClass);
+  }
+};
+
+
+const setEventListeners = (formElement, validationSettings) => {
+  const inputList = Array.from(formElement.querySelectorAll(validationSettings.inputSelector));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      setInputTouchedState(inputElement, validationSettings);
+      updateInputValidity(formElement, inputElement, validationSettings);
+      updateButtonState(formElement, validationSettings);
+    });
+  });
+};
+
+const updateFormValidity = (formElement, validationSettings) => {
+  const inputList = Array.from(formElement.querySelectorAll(validationSettings.inputSelector));
+  inputList.forEach((inputElement) => {
+    updateInputValidity(formElement, inputElement, validationSettings);
+  });
+  updateButtonState(formElement, validationSettings);
+}
+
+const enableValidation = (validationSettings) => {
+  const formList = Array.from(document.querySelectorAll(validationSettings.formSelector));
+  formList.forEach((formElement) => {
+    setEventListeners(formElement, validationSettings);
+  });
+}
+
+const prepareFormForUserInput = (formElement, validationSettings) => {
+  clearAllFormInputsTouchedState(formElement, validationSettings);
+  updateFormValidity(formElement, validationSettings);
+}
